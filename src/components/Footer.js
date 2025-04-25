87dd7398d3dcf5387f8b5223de172b745d0c45cf
codingacharya <kandadithirupathi4@gmail.com>
2025-04-25 14:11:15 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 codingacharya. All Rights Reserved.</p>
+      <p>Contact: kandadithirupathi4@gmail.com</p>
+      <p>Blog: codingacharya.blogspot.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
